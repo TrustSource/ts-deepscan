dce094bbd21f65104de3d90fbb40f5c62e8364d1
JTH <git4jth@eacg.de>
2025-02-12 12:58:30 +0100

    modified setup.py updating dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ts-deepscan',
     python_requires='>=3.8',
-    packages=[
-        'ts_deepscan',
-        'ts_deepscan.analyser',
-        'ts_deepscan.analyser.scancode',
-        'ts_deepscan.analyser.textutils',
-        'ts_deepscan.commentparser',
-        'ts_deepscan.scanner'
-    ],
+    packages=find_packages(),
     version='2.3.0',
     description='Repository scanner for the identification of effective licenses and copyright information.',
     author='EACG GmbH',
@@ -27,7 +20,7 @@ setup(
         'requests==2.32.3',
         'osadl_matrix',
         'pyminr>=0.1.1',
-        'scancode-toolkit==32.3.0',
+        'scancode-toolkit-mini==32.3.0',
         'dataclasses-json',
         'gitignore-parser==0.1.11',
         "tqdm==4.66.3",
